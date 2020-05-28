8b1484af16db50d083fddfb43bb705cf24ad6dfd
Rachel S. Lee <rslee0025@gmail.com>
2020-05-28 03:09:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- 
- famous_cats << "Duchess"
- famous_cats.inspect
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
